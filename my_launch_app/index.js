70858bfc016b558ea659058964d4549e5bcfe3bd
= <=>
2022-05-01 01:25:30 -0500

    Paso 6: Se importa la clase MisionCommander, y se instancia en index, creamos 3 objetos

--- my_launch_app/index.js
+++ my_launch_app/index.js
@@ -1,1 +1,13 @@
-//
+//importamos la clase del archivo app/missionCommander
+const MissionCommander = require('./app/missionCommander');
+
+//instanciamos tres objetos
+const Fer =new MissionCommander("Fernanda")
+console.log(Fer.name)
+
+const rodri =new MissionCommander("Rodrigo")
+console.log(rodri.name)
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
