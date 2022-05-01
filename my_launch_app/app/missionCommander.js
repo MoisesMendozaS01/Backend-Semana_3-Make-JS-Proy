3277234a86a98bbd9687cff21b47d02bbe936b57
= <=>
2022-05-01 01:03:01 -0500

    Paso 4: Creando archivo Mission comander en carpeta app  y creando una clase

---
+++ my_launch_app/app/missionCommander.js
@@ -0,0 +1,9 @@
+//Creando clase mission commander
+class MissionCommander {
+    constructor(name){
+        this.name=name
+    }
+}
+
+//esta linea nos permite exportat nuestra clase
+module.exports = MissionCommander
